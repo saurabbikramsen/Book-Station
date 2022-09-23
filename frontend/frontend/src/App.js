f062d4b209fee3f40f7eb27e6ae0a4993c036f0b
saurabbikramsen <113790283+saurabbikramsen@users.noreply.github.com>
2022-09-23 12:39:25 +0545

    frontend files upload
    frontend files upload

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">BookStation</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  );
+}
+
+export default App;
